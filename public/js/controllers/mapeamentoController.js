06c6c2acb55fa383343cf5f2f588d533c50d3c71
marcelobrgomes <marcelobrgomes@gmail.com>
2016-09-12 21:47:50 -0300

    markers

--- public/js/controllers/mapeamentoController.js
+++ public/js/controllers/mapeamentoController.js
@@ -1,23 +1,90 @@
 angular.module("arca").controller("MapeamentoController", function($scope, $timeout){
     $scope.map;
-        $scope.markers = [];
-        $scope.markerId = 1;
+    $scope.markers = [];
+    $scope.markerId = 1;
+    
+    var cities = [
+              {
+                  city : 'India',
+                  desc : 'This is the best country in the world!',
+                  lat : 23.200000,
+                  long : 79.225487
+              },
+              {
+                  city : 'New Delhi',
+                  desc : 'The Heart of India!',
+                  lat : 28.500000,
+                  long : 77.250000
+              },
+              {
+                  city : 'Mumbai',
+                  desc : 'Bollywood city!',
+                  lat : 19.000000,
+                  long : 72.90000
+              },
+              {
+                  city : 'Kolkata',
+                  desc : 'Howrah Bridge!',
+                  lat : 22.500000,
+                  long : 88.400000
+              },
+              {
+                  city : 'Chennai  ',
+                  desc : 'Kathipara Bridge!',
+                  lat : 13.000000,
+                  long : 80.250000
+              },
+              {
+                  city : 'Duque de Caxias  ',
+                  desc : 'teste',
+                  lat : -22.789755,
+                  long : -43.304798
+              }
+          ];
     
     $timeout(function(){
-       var latlng = new google.maps.LatLng(35.7042995, 139.7597564);
-            var myOptions = {
-                zoom: 8,
-                center: latlng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            $scope.map = new google.maps.Map(document.getElementById("map"), myOptions); 
-            $scope.overlay = new google.maps.OverlayView();
-            $scope.overlay.draw = function() {}; // empty function required
-            $scope.overlay.setMap($scope.map);
-            $scope.element = document.getElementById('map');
-           /* $scope.hammertime = Hammer($scope.element).on("hold", function(event) {
-                $scope.addOnClick(event);
-            });*/
+       var latlng = new google.maps.LatLng(-22.789755, -43.304798);
+        var myOptions = {
+            zoom: 17,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        
+        $scope.map = new google.maps.Map(document.getElementById("map"), myOptions); 
+        $scope.overlay = new google.maps.OverlayView();
+        $scope.overlay.draw = function() {}; // empty function required
+        $scope.overlay.setMap($scope.map);
+        $scope.element = document.getElementById('map');
+        
+              var infoWindow = new google.maps.InfoWindow();
+              
+              var createMarker = function (info){
+                  
+                  var marker = new google.maps.Marker({
+                      map: $scope.map,
+                      position: new google.maps.LatLng(info.lat, info.long),
+                      title: info.city
+                  });
+                  marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+                  
+                  google.maps.event.addListener(marker, 'click', function(){
+                      infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                      infoWindow.open($scope.map, marker);
+                  });
+                  
+                  $scope.markers.push(marker);
+                  
+              }  
+              
+              for (i = 0; i < cities.length; i++){
+                  createMarker(cities[i]);
+              }
+
+              $scope.openInfoWindow = function(e, selectedMarker){
+                  e.preventDefault();
+                  google.maps.event.trigger(selectedMarker, 'click');
+              }
+       
     }, 100);
     
 });
