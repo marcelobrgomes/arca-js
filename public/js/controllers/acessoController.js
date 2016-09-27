58c143251838f5f87ae154530220433209708dc9
marcelobrgomes <marcelobrgomes@gmail.com>
2016-09-26 22:04:22 -0300

    continuacao

---
+++ public/js/controllers/acessoController.js
@@ -0,0 +1,5 @@
+var app = angular.module("arca");
+
+app.controller("AcessoController", function($scope) {
+    
+});
--- public/js/controllers/indexController.js
+++ public/js/controllers/indexController.js
@@ -1,6 +1,3 @@
 angular.module("arca").controller("IndexController", function($scope){
-    $scope.scripts = [];
-    
-    //Buscar do lado servidor
-    $scope.scripts.push({src:"js/teste.js"});
+    $scope.templateAcesso = "partials/formAcesso.html";
 });
